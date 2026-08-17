cdd407057a21ea80685d6253b953ceef09bd2e52
thantzin1843 <thantzinwin1843@gmail.com>
2026-08-17 09:11:08 +0630

    Fix the ci fail

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,7 @@ describe("GET /", () => {
     const response = await request(app).get("/");
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe("Hello World!");
+    expect(response.body.message).toBe("Hello from Node.js!");
   });
 });
 
