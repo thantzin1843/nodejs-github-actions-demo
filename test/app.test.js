2d5fdfd73c027a2737efc8520c440ab9d4b731e5
thantzin1843 <thantzinwin1843@gmail.com>
2026-08-17 10:17:41 +0630

    ci cd deploy to render

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,7 @@ describe("GET /", () => {
     const response = await request(app).get("/");
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe("Hello from Node.js app!");
+    expect(response.body.message).toBe("Helo, CI/CD from Node.js app!");
   });
 });
 
